96b4023d1defbbdf17474e6554f2eb1decef8ddb
BrunaKimura <brunamk@al.insper.edu.br>
2017-09-13 17:27:36 -0300

    arrumei as funçoes de divisão

--- Proj-1-Comunicacao/0-COM-LoopBack/src/endescapsulamento.py
+++ Proj-1-Comunicacao/0-COM-LoopBack/src/endescapsulamento.py
@@ -113,35 +113,20 @@ class Empacotamento():
         return ''.join(result)
 
     def mod2div(self, divident, divisor):
-     
-        # Number of bits to be XORed at a time.
+
         pick = len(divisor)
-    
-        # Slicing the divident to appropriate
-        # length for particular step
         tmp = divident[0 : pick]
     
         while pick < len(divident):
     
             if tmp[0] == '1':
-    
-                # replace the divident by the result
-                # of XOR and pull 1 bit down
                 tmp = self.xor(divisor, tmp) + divident[pick]
     
-            else:   # If leftmost bit is '0'
-                # If the leftmost bit of the dividend (or the
-                # part used in each step) is 0, the step cannot
-                # use the regular divisor; we need to use an
-                # all-0s divisor.
+            else: 
                 tmp = self.xor('0'*pick, tmp) + divident[pick]
     
-            # increment pick to move further
             pick += 1
     
-        # For the last n bits, we have to carry it out
-        # normally as increased value of pick will cause
-        # Index Out of Bounds.
         if tmp[0] == '1':
             tmp = self.xor(divisor, tmp)
         else:
@@ -149,23 +134,16 @@ class Empacotamento():
     
         checkword = tmp
         return checkword
- 
-# Function used at the sender side to encode
-# data by appending remainder of modular divison
-# at the end of data.
+
     def encodeData(self, data, key):
     
         l_key = len(key)
-    
-        # Appends n-1 zeroes at end of data
+
         appended_data = data + '0'*(l_key-1)
         remainder = self.mod2div(appended_data, key)
     
-        # Append remainder in the original data
         codeword = data + remainder
-        # print("Remainder : ", remainder)
-        # print("Encoded Data (Data + Remainder) : ",
-        #     codeword)
+
         return remainder
         
     def getKey(self):
